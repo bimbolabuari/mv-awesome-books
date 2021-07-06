a00140b0cd14991b01a5e7eb174f25b1676c7d7a
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-07-06 01:54:47 +0100

    feat: Add remove function and refactor code to ES6 syntax

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# mv-awesome-books
+# HTML-CAPSTONE-PROJECT
+
+> This project is a Microverse exercise on Javascript DOM manipulation, event listener, arrow function, let and const, Javascript Object, Arrays, String interpolation, local storage.
+
+
+![screenshot]()
+
+## Built With
+
+- Semantic HTML
+- JavaScript
+- Github workflow
+- Linters(Lighthouse, stylelint, Webhint, eslint)
+
+## Live Preview
+
+[Github deploy Page]()
+
+## Author
+
+👤 **Abimbola Aminat Buari**
+
+- GitHub: [bimbolabuari](https://github.com/bimbolabuari)
+- Twitter: [bimbolabuari](https://twitter.com/bimbolabuari)
+- LinkedIn: [bimbolabuari](https://linkedin.com/in/bimbolabuari)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Microverse
+- Code reviewer(s).
+
+## 📝 License
+
+[MIT licensed](./LICENSE).
--- index.js
+++ index.js
@@ -1,52 +1,82 @@
+const generateId = () => (Math.random() + 1).toString(36).substring(7);
+
 function Book(title, author) {
-    this.title = title,
-    this.author = author
+  this.title = title;
+  this.author = author;
+  this.id = generateId();
 }
 
-function generateId() {
-    return (Math.random() + 1).toString(36).substring(7);
-  }
-
-const firstBook = new Book('Crime', 'Dostoyevsky')
-const secondBook = new Book('The Devil', 'Bulgakov')
+const firstBook = new Book('Crime', 'Dostoyevsky');
+const secondBook = new Book('The Devil', 'Bulgakov');
 
 let bookArray = [firstBook, secondBook];
 
-const bookCollection = document.querySelector('#collection')
-const createBook = document.querySelector('#createBook')
+const bookCollection = document.querySelector('#collection');
 
-function createBookElement(book) {
-    const bookContainer = document.createElement('div');
-    bookContainer.innerHTML = `
+const createBookElement = (book) => {
+  const bookContainer = document.createElement('div');
+  bookContainer.id = `${book.id}`;
+  bookContainer.innerHTML = `
     <h2>${book.title}</h2>
     <h3>${book.author}</h3>
-    <button type="submit">Remove</button>
+    <button type="button" data-action="delete" data-book-id="${book.id}">Remove</button>
     `;
-    return bookContainer;
-}
+  return bookContainer;
+};
 
-function displayBook(book, bookCollection) {
- const newBookElement = createBookElement(book);
+const displayBook = (book, bookCollection) => {
+  const newBookElement = createBookElement(book);
   bookCollection.appendChild(newBookElement);
-}
+};
 
-bookArray.forEach(book => {
-    displayBook(book, bookCollection);
-})
+bookArray.forEach((book) => {
+  displayBook(book, bookCollection);
+});
 
-const titleValue = document.querySelector('#title')
-const authorValue = document.querySelector('#author')
-const form = document.querySelector('#form')
+const titleValue = document.querySelector('#title');
+const authorValue = document.querySelector('#author');
+const form = document.querySelector('#form');
 
-function addBookToLibrary(event) {
-    event.preventDefault();
+const addBookToLibrary = (event) => {
+  event.preventDefault();
   const titleBook = titleValue.value;
   const authorBook = authorValue.value;
-  const newBookObject = new Book(titleBook, authorBook)
+  const newBookObject = new Book(titleBook, authorBook);
   bookArray.push(newBookObject);
-  displayBook(newBookObject, bookCollection)
-}
+  displayBook(newBookObject, bookCollection);
+};
 
 form.addEventListener('submit', addBookToLibrary);
 
-addBookToLibrary();
+const deleteBook = (id) => {
+  const filteredBook = bookArray.filter((book) => book.id !== id);
+  const deleteBookArticle = document.getElementById(`${id}`);
+  deleteBookArticle.remove();
+  bookArray = filteredBook;
+};
+
+document.addEventListener('click', (event) => {
+  if (!event.target.dataset.action) {
+    return;
+  }
+
+  const { bookId } = event.target.dataset;
+
+  if (event.target.dataset.action === 'delete') {
+    deleteBook(bookId);
+  }
+});
+
+const storeValue = () => {
+  const formData = {
+    name: titleValue.value,
+    owner: authorValue.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
+};
+
+form.addEventListener('input', storeValue);
+const newFormData = JSON.parse(localStorage.getItem('formData'));
+
+form.title.value = newFormData.name;
+form.author.value = newFormData.owner;
